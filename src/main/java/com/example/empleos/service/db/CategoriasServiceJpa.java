8f0d15aeba22d162c12f1892b3b72527373146e7
Hannia237 <hch06263@gmail.com>
2022-10-07 22:04:31 -0600

    Videos 1-3

---
+++ src/main/java/com/example/empleos/service/db/CategoriasServiceJpa.java
@@ -0,0 +1,28 @@
+package com.example.empleos.service.db;
+
+import java.util.List;
+
+import com.example.empleos.model.Categoria;
+import com.example.empleos.service.ICategoriasService;
+
+public class CategoriasServiceJpa implements ICategoriasService {
+
+	@Override
+	public void guardar(Categoria categoria) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Categoria> buscarTodas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Categoria buscarPorId(Integer idCategoria) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
