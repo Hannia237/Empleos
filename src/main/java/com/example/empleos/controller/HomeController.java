64e5ffaae88b9cdb4c26665bc9fd58b593022624
Hannia237 <hch06263@gmail.com>
2022-10-11 22:08:53 -0600

    Video 10-12

--- src/main/java/com/example/empleos/controller/HomeController.java
+++ src/main/java/com/example/empleos/controller/HomeController.java
@@ -12,13 +12,16 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.empleos.model.Perfil;
@@ -39,6 +42,10 @@ public class HomeController {
 	
 	@Autowired
 	private IUsuariosService serviceUsuarios;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	
 	@GetMapping("/tabla")
 	public String mostrarTabla(Model model) {
@@ -104,7 +111,10 @@ public class HomeController {
 	
 	@PostMapping("/signup")
 	public String guardarRegistro(Usuario usuario, RedirectAttributes attributes) {
-		
+		 
+		 String pwdPlano = usuario.getPassword();
+		 String pwdEncriptado = passwordEncoder.encode(pwdPlano);
+		 usuario.setPassword(pwdEncriptado);
 		 usuario.setEstatus(1);
 		 usuario.setFechaRegistro(new Date());
 		 
@@ -130,6 +140,12 @@ public class HomeController {
 		List<Vacante> lista = serviceVacantes.buscarByExample(example);
 		model.addAttribute("vacantes", lista);
 		return "home";
+	}
+	
+	@GetMapping("/bcrypt/{texto}")
+	@ResponseBody
+	public String encriptar(@PathVariable("texto") String texto) {
+		return texto + "Encriptado en Bcrypt: " + passwordEncoder.encode(texto);
 	}
 	
 	/**
